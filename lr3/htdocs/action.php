46b2630c27221e60113c4238114bd73ebad2624f
Rikishi_02 <84096779+Rikishi1742@users.noreply.github.com>
2022-11-24 20:56:55 +0300

    Add files via upload

---
+++ lr3/htdocs/action.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="action_handler.php" method="POST">
+        <dl>
+        <dt>Имя:
+        <dd><input type="text" name="name">
+        <dt>Адресс email:
+        <dd><input type="text" name="mail"></dd>
+        <dt>Примечание:
+        <dd><textarea name="comment" id="" cols="20" rows="5"></textarea>
+        </dl>
+        <p><input type="submit"></p>
+    </form>
+
+</body>
+</html>
