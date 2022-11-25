57208c09e88d267343520632a567fe3a8313bbe5
Rikishi_02 <84096779+Rikishi1742@users.noreply.github.com>
2022-11-25 08:52:59 +0300

    Add files via upload

---
+++ 2/3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $filestream = fopen('3.txt','r') or die('Невозможно открыть файл!');
+    while($char = fgetc($filestream))
+    {echo $char.'*';}
+    fclose($filestream);
+    ?>
+</body>
+</html>
